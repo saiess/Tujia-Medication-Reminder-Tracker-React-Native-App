a3b9d22e4b4f838ae4f434c5ed2dcd70f28285ad
saiess <saidessalami0@gmail.com>
2022-06-22 03:15:30 +0100

    updates

--- tujia-client-mobile/screens/ShowData.tsx
+++ tujia-client-mobile/screens/ShowData.tsx
@@ -1,44 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import tw from 'twrnc';
 import axios from 'axios';
-
-let baseUrl = 'https://www.breakingbadapi.com/api/characters/';
+let baseUrl = 'http://192.168.1.3:4000/medications/';
 const ShowData = () => {
-      const [medication, setMedication] = useState(null);
-      const [isLoading, setIsLoading] = useState(false);
-      const [hasError, setErrorFlag] = useState(false);
+  const [medication, setMedication] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setErrorFlag] = useState(false);
 
-      useEffect(() => {
-            const fetchData = async () => {
-                    setIsLoading(true);
-                    setErrorFlag(false);
-                    try {
-                            const response = await axios.get(baseUrl);
-                            setMedication(response.data);
-                            // console.warn(response.data[0].name);
-                    } catch (error) {
-                            setErrorFlag(true);
-                    }
-                    setIsLoading(false);
-            }
-            fetchData();
-        }, []);
+  const splitMedicationName = (p: string) => {
+    let vv = p.replaceAll(',', ' ');
+    return vv.split(' ').slice(0, 2).join(' ').toLowerCase();
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setErrorFlag(false);
+      try {
+        const response = await axios.get(baseUrl);
+        setMedication(response.data);
+        console.log(response.data[0].name);
+      } catch (error) {
+        setErrorFlag(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
   return (
     <View
       style={tw`w-full h-full rounded-l-xl bg-slate-200 flex items-center justify-center`}
     >
       <FlatList
         data={medication}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           // @ts-ignore
           <TouchableOpacity
             style={tw`w-full h-16 rounded-lg mt-8 flex flex-row bg-slate-50 shadow-sm items-center`}
             // @ts-ignore
             onPress={() => navigation.navigate('ShowData')}
+            key={index}
           >
             <View style={tw`w-16 h-full rounded-l-xl bg-slate-700`} />
-            <Text style={tw`text-base font-semibold ml-6`}>{item.name}</Text>
+            <Text style={tw`text-base font-semibold ml-6`}>
+              {splitMedicationName(item.name)}
+            </Text>
           </TouchableOpacity>
         )}
         keyExtractor={(item) => item.id}
